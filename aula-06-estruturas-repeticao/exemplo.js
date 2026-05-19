efa71834d9ee367c06cdf57547b5228bb0886f4c
senaisesisa1igds <senaisesisa1igds@gmail.com>
2026-05-19 12:29:32 -0300

    Exemplo
    Laço FOR.

---
+++ aula-06-estruturas-repeticao/exemplo.js
@@ -0,0 +1,17 @@
+// Rodar para usar módulo
+// npm install prompt-sync
+
+const prompt = require('prompt-sync')()
+
+console.log('---Repetição de versos---')
+console.log("")
+
+// Estrutura de repetição (Laço FOR)
+for (let i = 1; i <=5; i++) {
+    console.log("Volta o cão arrependido...")
+    console.log("Com suas orelhas fartas...")
+    console.log("Com seu osso roído...")
+    console.log("E o rabo entre as patas!")
+    console.log("-------------------------")
+}
+
