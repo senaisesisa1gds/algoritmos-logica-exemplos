23013b7c510ff929caea3f448080d1d3faf3ebf3
senaisesisa2hds <materialdevsenai@gmail.com>
2026-06-02 10:22:31 -0300

    Exemplo 3
    Aula 6 exemplo 3 feito.

--- aula-06-estruturas-repeticao/ex3.js
+++ aula-06-estruturas-repeticao/ex3.js
@@ -1,1 +1,24 @@
+// Chamada da biblioteca
 const prompt = require('prompt-sync')()
+
+// Perguntar para o usuário qual o número
+let numero = Number(prompt('Digite um número positivo: '))
+
+// variáveis auxiliares para poder calcular
+let contador = 1 // Variável para contagem
+let soma = 0 // Variável para guardar o valor dos números somados.
+
+// Laço ou "LOOP" para fazer a soma
+// Enquanto o contador for menor ou igual ao número digitado, 
+// o bloco será repetido
+while (contador <= numero) {
+    soma += contador
+    // soma = soma + contador
+
+    contador++
+    // contador = contador + 1
+}
+
+// Exibindo os resultados
+console.log("Resultado da soma: ", soma)
+// console.log(`Resultado da soma:  ${soma}`)
